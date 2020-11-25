8542fb80db37f0e2171379c11bb63c1f9ed92965
eeeeeeeelias <eliasnwr@gmail.com>
2020-11-25 19:17:00 +0300

    added start view

--- codenames/views.py
+++ codenames/views.py
@@ -1,3 +1,34 @@
+"""
+Views for codenames app.
+"""
+
 from django.shortcuts import render
 
-# Create your views here.
+from .consts import CURRENT_CUP_NUMBER
+from .models import Group
+
+
+def start_view(request):
+    """
+    Start view of tounament website.
+    """
+    groups = [g.name for g in Group.objects.filter(
+        cup_id__number=CURRENT_CUP_NUMBER)]
+    context = {
+        "group_names": groups
+    }
+    return render(request, "codenames/start_page.html", context)
+
+
+def all_groups_tables_view(request):
+    """
+    View with tables of all groups.
+    """
+    raise NotImplementedError
+
+
+def one_group_table_view(request):
+    """
+    View with table of one group.
+    """
+    raise NotImplementedError
