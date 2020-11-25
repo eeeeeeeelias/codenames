a03277a887b30e860dd2778163e58c7266aeb075
eeeeeeeelias <eliasnwr@gmail.com>
2020-11-25 19:16:27 +0300

    fixed url

--- codenames/urls.py
+++ codenames/urls.py
@@ -5,10 +5,12 @@ Urls for codenames app.
 from django.urls import path
 from . import views
 
+app_name = "codenames"
+
 urlpatterns = [
     path("", views.start_view, name="index"),
     path("results/", views.all_groups_tables_view, name="all_groups_tables"),
-    path("results/<str:group_name>",
+    path("results/<str:group_name>/",
          views.one_group_table_view,
          name="one_group_table"),
 ]
