445d3ddff70d68426b977521a8f86991aef556aa
eeeeeeeelias <eliasnwr@gmail.com>
2020-11-27 21:15:00 +0300

    Fill many cells for result table from db
    * Add classes argument to HtmlTableCell for multiple css classes
    * Make HtmlTableCell give multiple css classes with space delimiter
    * Get game result for result table from db
    * Get word difference for result table from db

--- codenames/table_render.py
+++ codenames/table_render.py
@@ -2,10 +2,12 @@
 Rendering functions for future html tables
 """
 
+import copy
 import typing as tp
 
 from django.forms.models import model_to_dict
 
+from .consts import get_score_str
 from .models import GameResult, Group, ResultType, Team
 
 
@@ -13,13 +15,24 @@ class HtmlTableCell:
     def __init__(self,
                  *,
                  class_: str = None,
+                 classes: tp.Optional[tp.List[str]] = None,
                  content: tp.Optional[str] = None,
                  title: tp.Optional[str] = None):
+        if class_ is not None and classes is not None:
+            raise TypeError("please specify either class_ or classes")
+        self._classes: tp.List[str] = []
+        if class_ is not None:
+            self._classes.append(class_)
+        if classes is not None:
+            self._classes = copy.deepcopy(classes)
         self.content: str = ""
         if content:
             self.content = content
-        self.class_: tp.Optional[str] = class_
         self.title: tp.Optional[str] = title
+
+    @property
+    def class_(self):
+        return " ".join(self._classes)
 
     def __repr__(self):
         return (f"HtmlTableCell("
@@ -52,6 +65,40 @@ def render_result_table_header(group: Group) -> None:
     ]
 
 
+def get_gameresult_cell(game_result: GameResult,
+                        *,
+                        words_difference,
+                        is_team_home: bool) -> HtmlTableCell:
+    """
+    words_difference is in AND out!
+    """
+    title: tp.Optional[str] = None
+    if game_result.result_type.is_auto:
+        title = game_result.result_type.description
+
+    classes: tp.List[str] = ["gameresult_cell"]
+    if game_result.result_type.is_home_win == is_team_home:
+        # Win
+        if game_result.result_type.is_auto:
+            # Do not count auto_win wd
+            classes.append("auto_win")
+        else:
+            words_difference["for"] += game_result.absolute_score
+    else:
+        # Lose
+        if game_result.result_type.is_auto:
+            classes.append("auto_lose")
+        # Do count auto lose wd!
+        words_difference["against"] += game_result.absolute_score
+
+    return HtmlTableCell(
+        classes=classes,
+        content=get_score_str(
+            game_result.home_score if is_team_home else game_result.away_score
+        ),
+        title=title)
+
+
 def get_row(seed, team, group, num_teams):
     home_games = GameResult.objects.filter(home_team=team, group=group)
     away_games = GameResult.objects.filter(away_team=team, group=group)
@@ -65,7 +112,26 @@ def get_row(seed, team, group, num_teams):
     home_loses = home_games.filter(result_type__in=away_win_result_types)
     away_loses = away_games.filter(result_type__in=home_win_result_types)
 
-    # print(" ".join(f"{x.count()}" for x in [home_wins, home_loses, away_wins, away_loses]))
+    result_subrow: tp.List[HtmlTableCell] = [
+        HtmlTableCell(class_=("itself_cell" if seed == rival_seed else None))
+                      for rival_seed in range(num_teams)
+    ]
+
+    words_difference = {"for": 0, "against": 0}
+
+    # TODO: process game results that are not is_finished
+    for hg in home_games:
+        rival_seed = hg.away_team.seed
+        result_subrow[rival_seed] = get_gameresult_cell(
+            hg,
+            is_team_home=True,
+            words_difference=words_difference)
+    for ag in away_games:
+        rival_seed = ag.home_team.seed
+        result_subrow[rival_seed] = get_gameresult_cell(
+            ag,
+            is_team_home=False,
+            words_difference=words_difference)
 
     won = home_wins.count() + away_wins.count()
     lost = home_loses.count() + away_loses.count()
@@ -73,14 +139,16 @@ def get_row(seed, team, group, num_teams):
     return [
         HtmlTableCell(class_="place_cell", content=f"{seed + 1}"),
         HtmlTableCell(class_="team_cell", content=f"{team.short}"),
-        *[HtmlTableCell(class_="gameresult_cell", content=f"TODO")
-          for place in range(num_teams)],  # TODO: add game_result getting
+        *result_subrow,
         HtmlTableCell(class_="played_cell", content=f"{played}"),
         HtmlTableCell(class_="won_cell", content=f"{won}"),
         HtmlTableCell(class_="lost_cell", content=f"{lost}"),
-        HtmlTableCell(class_="wd_cell", content=f"TODO",
-                      title="Words difference"),  # TODO: add wd getting
-        HtmlTableCell(class_="fouls_cell", content=f"TODO"),  # TODO: add fouls getting
+        HtmlTableCell(
+            class_="wd_cell",
+            content=f"{words_difference['for']-words_difference['against']:+}",
+            title="Words difference"),  # TODO: add wd getting
+        # TODO: add fouls getting
+        HtmlTableCell(class_="fouls_cell", content=f"TODO"),
     ]
 
 
