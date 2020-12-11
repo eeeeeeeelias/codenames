279aa85f9b0a187b42a42dcaf6c762b068b65a4a
eeeeeeeelias <eliasnwr@gmail.com>
2020-12-11 16:08:04 +0300

    trying to fix deploy

--- cn_web/settings.py
+++ cn_web/settings.py
@@ -137,3 +137,6 @@ DATABASES["default"].update(db_from_env)
 
 STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 
+import django_heroku
+django_heroku.settings(locals())
+
