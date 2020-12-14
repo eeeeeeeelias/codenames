1d9111cf86d0779b828122b318535f1e13afacb3
eeeeeeeelias <eliasnwr@gmail.com>
2020-12-14 17:28:35 +0300

    feat: Add multiple arenas with one command

--- codenames/management/commands/add_arenas.py
+++ codenames/management/commands/add_arenas.py
@@ -17,22 +17,24 @@ class Command(BaseCommand):
 
     def add_arguments(self, parser):
         parser.add_argument("-g", "--group_name", type=str, required=True)
-        parser.add_argument("-a", "--arena_number", type=int, required=True)
+        parser.add_argument("-a", "--arena_number",
+                            action="append", type=int, required=True)
 
     def handle(self, *args, **options):
         group = Group.objects.get(name=options["group_name"],
                                   cup__number=CURRENT_CUP_NUMBER)
-        params = {
-            "group": group,
-            "number": options["arena_number"]
-        }
-        try:
-            existing_arena = Arena.objects.get(**params)
-        except Arena.DoesNotExist:
-            existing_arena = None
-        if existing_arena is not None:
-            self.stdout.write(f"Arena {existing_arena} already exists")
-            return
-        new_arena = Arena(**params)
-        new_arena.save()
-        self.stdout.write(f"Arena {new_arena} saved")
+        for arena_number in options["arena_number"]:
+            params = {
+                "group": group,
+                "number": arena_number
+            }
+            try:
+                existing_arena = Arena.objects.get(**params)
+            except Arena.DoesNotExist:
+                existing_arena = None
+            if existing_arena is not None:
+                self.stdout.write(f"Arena {existing_arena} already exists")
+                continue
+            new_arena = Arena(**params)
+            new_arena.save()
+            self.stdout.write(f"Arena {new_arena} saved")
